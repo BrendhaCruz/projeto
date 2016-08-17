afdb6826a6be4f8e82225afaaac4ff1d00667bdb
Andre Yuri <andrecg16@gmail.com>
2016-08-17 17:12:18 -0300

    first commit

---
+++ app/src/main/java/les/projeto/quebra_galho/MainActivity.java
@@ -0,0 +1,19 @@
+package les.projeto.quebra_galho;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
