2faa82cfe22666da6fbc6ee8e66d01e6c71cfa2b
Brendha Cruz <Brendha Cruz>
2016-08-23 09:58:10 -0300

    mudanca tela inicial

--- app/src/main/java/les/projeto/quebra_galho/MainActivity.java
+++ app/src/main/java/les/projeto/quebra_galho/MainActivity.java
@@ -17,12 +17,72 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final ImageView iv1 = (ImageView) findViewById(R.id.iv1);
+        final ImageView iv2 = (ImageView) findViewById(R.id.iv2);
+        final ImageView iv3 = (ImageView) findViewById(R.id.iv3);
+        final ImageView iv4 = (ImageView) findViewById(R.id.iv4);
+        final ImageView iv5 = (ImageView) findViewById(R.id.iv5);
+        final ImageView iv6 = (ImageView) findViewById(R.id.iv6);
 
         iv1.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                iv1.setImageResource(R.drawable.icone);
+                iv1.setImageResource(R.drawable.eletrico);
+                Intent intent = new Intent(MainActivity.this, TutoriaisPinturaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        iv2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                iv2.setImageResource(R.drawable.encanador);
+                Intent intent = new Intent(MainActivity.this, TutoriaisPinturaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        iv3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                iv3.setImageResource(R.drawable.marceneiro);
+                Intent intent = new Intent(MainActivity.this, TutoriaisPinturaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        iv4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                iv4.setImageResource(R.drawable.mecanico);
+                Intent intent = new Intent(MainActivity.this, TutoriaisPinturaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        iv5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                iv5.setImageResource(R.drawable.pedreiro);
+                Intent intent = new Intent(MainActivity.this, TutoriaisPinturaActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        iv6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                iv6.setImageResource(R.drawable.diversos);
                 Intent intent = new Intent(MainActivity.this, TutoriaisPinturaActivity.class);
                 startActivity(intent);
 
