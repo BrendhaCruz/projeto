08117291b925fca65c75e8fdb97826c531e9e9b3
Germano Gualberto <gmanopoliano@gmail.com>
2016-08-28 12:56:30 -0300

    criacao modelo usuario

---
+++ app/src/main/java/les/projeto/quebra_galho/model/Profissional.java
@@ -0,0 +1,7 @@
+package les.projeto.quebra_galho.model;
+
+/**
+ * Created by GermanoPRodriguesGua on 28/08/2016.
+ */
+public class Profissional {
+}
---
+++ app/src/main/java/les/projeto/quebra_galho/model/Usuario.java
@@ -0,0 +1,16 @@
+package les.projeto.quebra_galho.model;
+
+/**
+ * Created by GermanoPRodriguesGua on 28/08/2016.
+ */
+public class Usuario {
+    public String nome;
+    public String email;
+    //public String avaliacao;
+
+    public Usuario(String nome, String email, String avaliacao){
+        this.nome = nome;
+        this.email =email;
+      //  this.avaliacao = avaliacao;
+    }
+}
