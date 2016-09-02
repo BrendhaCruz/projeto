12b2d4314571cc91a176aa898b751876a05dca79
Brendha Cruz <Brendha Cruz>
2016-09-02 14:38:35 -0300

    criacao classe Profissional e SQL:

--- app/src/main/java/les/projeto/quebra_galho/model/Profissional.java
+++ app/src/main/java/les/projeto/quebra_galho/model/Profissional.java
@@ -1,7 +1,51 @@
 package les.projeto.quebra_galho.model;
 
-/**
- * Created by GermanoPRodriguesGua on 28/08/2016.
- */
 public class Profissional {
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String foto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
 }
+
---
+++ app/src/main/java/les/projeto/quebra_galho/model/ProfissionalSQL.java
@@ -0,0 +1,74 @@
+package les.projeto.quebra_galho.model;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import les.projeto.quebra_galho.model.Profissional;
+
+public class ProfissionalSQL extends SQLiteOpenHelper {
+
+    private static final int VERSAO = 2;
+    private static final String TABELA = "Profissional";
+    private static final String[] COLS = {"id", "nome", "telefone", "email", "foto"};
+
+    public ProfissionalSQL(Context context) {
+        super(context, TABELA, null, VERSAO);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE TABLE " + TABELA + " ");
+        sb.append("(id INTEGER PRIMARY KEY, ");
+        sb.append(" nome TEXT UNIQUE NOT NULL, ");
+        sb.append(" telefone TEXT, ");
+        sb.append(" site TEXT, ");
+        sb.append(" foto TEXT);");
+        db.execSQL(sb.toString());
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("DROP TABLE IF EXISTS " + TABELA);
+        db.execSQL(sb.toString());
+        onCreate(db);
+
+    }
+
+    public void inserir(Profissional profissional) {
+        ContentValues values = new ContentValues();
+        values.put("nome", profissional.getNome());
+        values.put("telefone", profissional.getTelefone());
+        values.put("email", profissional.getEmail());
+        values.put("foto", profissional.getFoto());
+
+        getWritableDatabase().insert(TABELA, null, values);
+    }
+
+    public List<Profissional> getLista(){
+        List<Profissional> profissionais = new ArrayList<Profissional>();
+
+        Cursor c = getWritableDatabase().query(TABELA,COLS, null, null, null, null, null);
+
+        while(c.moveToNext()){
+            Profissional profissional = new Profissional();
+            profissional.setId(c.getLong(0));
+            profissional.setNome(c.getString(1));
+            profissional.setTelefone(c.getString(2));
+            profissional.setEmail(c.getString(3));
+            profissional.setFoto(c.getString(4));
+            profissionais.add(profissional);
+        }
+        c.close();
+
+        return profissionais;
+    }
+}
