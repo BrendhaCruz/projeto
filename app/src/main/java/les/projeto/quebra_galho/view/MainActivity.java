a4eb2d6b1fad53bfc55fd32f951187a1e77bdd61
Gervásio Júnior <juniorb08@gmail.com>
2016-10-11 11:35:18 -0300

    Adiciona click listener das categorias

--- app/src/main/java/les/projeto/quebra_galho/view/MainActivity.java
+++ app/src/main/java/les/projeto/quebra_galho/view/MainActivity.java
@@ -1,5 +1,6 @@
 package les.projeto.quebra_galho.view;
 
+import android.content.Intent;
 import android.databinding.ObservableArrayList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,6 +10,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
+import android.util.Log;
+import android.view.View;
 
 import com.github.nitrico.lastadapter.LastAdapter;
 import com.google.firebase.database.DataSnapshot;
@@ -16,13 +19,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 import les.projeto.quebra_galho.BR;
 import les.projeto.quebra_galho.R;
 import les.projeto.quebra_galho.model.Categoria;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LastAdapter.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,54 +62,13 @@ public class MainActivity extends AppCompatActivity {
 
         LastAdapter.with(categoriesList, BR.categoria)
                 .map(Categoria.class, R.layout.single_category)
+                .onClickListener(this)
                 .into(categoriesView);
+    }
 
-        /*firebaseDatabase.getReference("categorias").child("5").child("imagem").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String file = dataSnapshot.getValue(String.class);
-                firebaseStorage.getReference("imagens/categorias/" + file).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                            Picasso.with(MainActivity.this).load(uri.toString()).into(new Target() {
-                                @Override
-                                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-                                    byte[] bytes = baos.toByteArray();
-                                    String base64Image = Base64.encodeToString(bytes, Base64.DEFAULT);
-
-                                    // we finally have our base64 string version of the image, save it.
-                                    firebaseDatabase.getReference("categorias").child("5").child("img").setValue(base64Image);
-                                }
-                                @Override
-                                public void onBitmapFailed(Drawable errorDrawable) {
-                                }
-                                @Override
-                                public void onPrepareLoad(Drawable placeHolderDrawable) {
-                                }
-                            });
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.i("ataga", e.toString());
-                    }
-                });
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });*/
-//        iv1.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, ListaTutorialActivity.class);
-//                intent.putExtra("categoria", 1);
-//                startActivity(intent);
-//            }
-//        });
+    @Override
+    public void onClick(@NotNull Object o, @NotNull View view, int i, int i1) {
+        startActivity(new Intent(MainActivity.this, ListaTutorialActivity.class).putExtra("categoria", i1));
     }
 }
 
